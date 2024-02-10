1a23f57c80386be9bc367047326fe3483116b4ff
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-02-09 18:55:05 -0700

    Set YARN_CACHE_FOLDER env variable

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -2,6 +2,10 @@
 
 echo "Starting the script..."
 
+# Set an environment variable YARN_CACHE_FOLDER=./.next/cache/yarn
+echo "Setting environment variable YARN_CACHE_FOLDER=./.next/cache/yarn..."
+export YARN_CACHE_FOLDER=./.next/cache/yarn
+
 # Run yarn install --immutable
 echo "Running 'yarn install --immutable'..."
 yarn install --immutable
